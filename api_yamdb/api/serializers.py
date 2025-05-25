73e343ad296d6089bd927a866cfd60e194ee3890
Егор Жура <egorzhyra086@gmail.com>
2025-05-25 04:51:27 +0300

    Исправленно

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from reviews.models import Category, Genre, Title
+from rest_framework.exceptions import NotFound
+
+from reviews.models import Review, Comment, Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -17,23 +19,83 @@ class GenreSerializer(serializers.ModelSerializer):
 class TitleReadSerializer(serializers.ModelSerializer):
     genre = GenreSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
+    rating = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Title
-        fields = '__all__'
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
 
 
 class TitleWriteSerializer(serializers.ModelSerializer):
     genre = serializers.SlugRelatedField(
-        queryset=Genre.objects.all(),
         slug_field='slug',
-        many=True
+        many=True,
+        queryset=Genre.objects.all()
     )
     category = serializers.SlugRelatedField(
-        queryset=Category.objects.all(),
-        slug_field='slug'
+        slug_field='slug',
+        queryset=Category.objects.all()
     )
+    rating = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Title
-        fields = '__all__'
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category', 'rating')
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Review."""
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        read_only_fields = ('author',)
+
+    def validate(self, data):
+        """Проверка на уникальность отзыва и существование title_id."""
+        if self.context['request'].method == 'POST':
+            title_id = self.context['view'].kwargs.get('title_id')
+            author = self.context['request'].user.id
+
+            # Проверка существования title_id
+            try:
+                Title.objects.get(pk=title_id)
+            except Title.DoesNotExist:
+                raise NotFound(
+                    'Произведение с указанным ID не существует'
+                )
+
+            # Проверка на уникальность отзыва
+            if Review.objects.filter(
+                    title_id=title_id,
+                    author=author
+            ).exists():
+                raise serializers.ValidationError(
+                    'Вы уже оставили отзыв на это произведение'
+                )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Comment."""
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
+        read_only_fields = ('author',)
+
+    def validate(self, data):
+        """Проверка существования review_id."""
+        if self.context['request'].method == 'POST':
+            review_id = self.context['view'].kwargs.get('review_id')
+            try:
+                Review.objects.get(pk=review_id)
+            except Review.DoesNotExist:
+                raise NotFound(
+                    'Отзыв с указанным ID не существует'
+                )
+        return data
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets, mixins, filters
-from rest_framework.permissions import IsAdminUser, SAFE_METHODS, BasePermission
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from reviews.models import Review, Comment, Category, Genre, Title
+from .serializers import ReviewSerializer, CommentSerializer
+from .permissions import IsAuthorOrModeratorOrReadOnly, IsAdminOrReadOnly
 
-from reviews.models import Category, Genre, Title
+from rest_framework import viewsets, mixins, filters
+
 from .serializers import (
     CategorySerializer,
     GenreSerializer,
@@ -10,12 +14,43 @@ from .serializers import (
 )
 
 
-class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in SAFE_METHODS or
-            request.user.is_authenticated and request.user.is_staff
-        )
+class ReviewViewSet(viewsets.ModelViewSet):
+    """ViewSet для модели Review."""
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrModeratorOrReadOnly]
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_queryset(self):
+        """Получение queryset для отзывов конкретного произведения."""
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        """Создание отзыва с автоматическим указанием автора."""
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title_id=title.id)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """ViewSet для модели Comment."""
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrModeratorOrReadOnly]
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_review(self):
+        """Получние отзыва, к которому относится(-ятся) комментарий(-и)"""
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return get_object_or_404(title.reviews,
+                                 id=self.kwargs.get('review_id'))
+
+    def get_queryset(self):
+        """Получение queryset для комментариев конкретного отзыва."""
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        """Создание комментария с автоматическим указанием автора."""
+        review = self.get_review()
+        serializer.save(author=self.request.user, review_id=review.id)
 
 
 class CategoryViewSet(
@@ -49,8 +84,30 @@ class GenreViewSet(
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
             return TitleReadSerializer
         return TitleWriteSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        category_slug = self.request.query_params.get('category')
+        genre_slug = self.request.query_params.get('genre')
+        name = self.request.query_params.get('name')
+        year = self.request.query_params.get('year')
+
+        if category_slug:
+            queryset = queryset.filter(category__slug=category_slug)
+        if genre_slug:
+            queryset = queryset.filter(genre__slug=genre_slug)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if year:
+            queryset = queryset.filter(year=year)
+
+        return queryset
